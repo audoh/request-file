b3aac97d4756022e55e83744dbb4c82e8c062753
Ashleigh Udoh <ash@tickitto.com>
2021-03-15 11:41:36 +0000

    updated request-file.sh to work!

--- bin/request-file.sh
+++ bin/request-file.sh
@@ -1,4 +1,21 @@
+log() {
+  if [ -z "$REQUESTFILEDEBUG" ]; then
+    return 0
+  fi
+  echo $@ >&2
+}
+
 SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
 ROOT_DIR="$SCRIPT_DIR/.."
-cd "$ROOT_DIR"
-PYTHONPATH="$ROOT_DIR/src" poetry run python -m request_file.main "$@"
+
+# Activate venv
+log Activating venv
+pushd "$ROOT_DIR" > /dev/null
+VENV_DIR=`poetry env info -p`
+popd > /dev/null
+source $VENV_DIR/bin/activate
+
+# Run script
+log Running script
+export PYTHONPATH="$ROOT_DIR/src"
+python -m request_file.main "$@"
