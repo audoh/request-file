224d44d3be9e9d6fd8b4a76e0ad7e5414cb02d69
Ashleigh Udoh <ash@audoh.co.uk>
2021-03-21 13:25:54 +0000

    refactors and improvements to the trailing space hack

--- bin/bash-completer.sh
+++ bin/bash-completer.sh
@@ -5,15 +5,15 @@ _requestfile() {
   shift 1
   local comp_words=("$@")
 
+  # Check args are in the format 0 WORD0 WORD1 WORD2
   [ -n "$comp_cword" ] && [ "$comp_cword" -ge 0 ] 2>/dev/null
   if [ $? -ne 0 ]; then
     echo error: arg 1 must be integer index of current word >&2
     return
   fi
 
-  local cur="${comp_words[comp_cword]}"
 
-  # Check we don't have --
+  # Check we don't have -- (end of options marker)
   local allow_opt=true
   local idx=0
   until [ "$idx" -eq "$comp_cword" ]; do
@@ -25,22 +25,23 @@ _requestfile() {
   done
   unset idx
 
+  local curword="${comp_words[comp_cword]}"
   if [ "$allow_opt" = true ]; then
     if [ "$comp_cword" -ge 1 ]; then
-      local prev="${comp_words[comp_cword-1]}"
+      local prevword="${comp_words[comp_cword-1]}"
 
-      if [ "$prev" == "--replace" ] || [ "$prev" == "-r" ]; then
+      if [ "$prevword" == "--replace" ] || [ "$prevword" == "-r" ]; then
         return
-      elif [ "$prev" == "--format" ] || [ "$prev" == "-f" ]; then
+      elif [ "$prevword" == "--format" ] || [ "$prevword" == "-f" ]; then
         local formats
         formats="$formats body"
         formats="$formats verbose"
         formats="$formats requests-mock"
-        compgen -W "$formats" -- "$cur"
+        compgen -W "$formats" -- "$curword"
         return
-      elif [ "$prev" == "--output" ] || [ "$prev" == "-o" ] || \
-          [ "$prev" == "--exports" ] || [ "$prev" == "-e" ]; then
-        compgen -f -- "$cur"
+      elif [ "$prevword" == "--output" ] || [ "$prevword" == "-o" ] || \
+          [ "$prevword" == "--exports" ] || [ "$prevword" == "-e" ]; then
+        compgen -f -- "$curword"
         return
       fi
     fi
@@ -52,28 +53,33 @@ _requestfile() {
     opts="$opts --print-exports -p"
     opts="$opts --output -o"
     opts="$opts --exports -e"
-    compgen -W "$opts" -- "$cur"
-
+    compgen -W "$opts" -- "$curword"
   fi
 
-  files=`compgen -f -- "$cur" | grep .json$`
-  dirs=`compgen -d -S / -- "$cur"`
+  files=`compgen -f -- "$curword" | grep .json$`
+  dirs=`compgen -d -S / -- "$curword"`
 
-  if [ `echo "$dirs" | wc -l` -eq 1 ] && [ -z "$files" ]; then
+
+  if [ -n "$dirs" ] && [ `echo "$dirs" | wc -l` -eq 1 ] && [ -z "$files" ]; then
     # Having no luck whatsoever with compopt to disable the trailing space and the documentation is minimal
-    # Making bash think it needs to stop to let you decide between the dir and some random gibberish works as well as anything as long as the directory is not empty
-    # If the directory is empty then the tab result will be whatever the gibberish is
-    # So the gibberish should be the same as the directory
-    echo "${dirs[0]}${dirs[0]}"
+    # Making bash think it needs to stop to let you decide between the dir and some random gibberish works as well as anything
+    # The reason this works is because although it's adding another result, once there is a trailing slash, compgen will look in that directory for results
+    # Tab-completion will therefore then move to the next folder, and the process will repeat until we either start finding files or $dirs becomes empty
+    echo "${dirs[0]}asdf"
   fi
 
   for file in "$files"; do
-    echo "$file"
+    if [ -n "$file" ]; then
+      echo "$file"
+    fi
   done
 
   for dir in "$dirs"; do
-    echo "$dir"
+    if [ -n "$dir" ]; then
+      echo "$dir"
+    fi
   done
 }
 
 _requestfile "$@"
+unset _requestfile
