6f53ab893d6973e5d7ce2afeee9bea35c4bbed55
Ashleigh Udoh <ash@audoh.co.uk>
2021-03-23 21:29:23 +0000

    bash completer tests fleshed out

--- tests/test_bash_completer.py
+++ tests/test_bash_completer.py
@@ -158,7 +158,86 @@ class TestTwoDirs(BaseTestCase):
         ("t", set()),
         ("o", {"one/", "other/"}),
         ("one", {"one/", BaseTestCase.get_trick_word("one/")}),
+        ("other", {"other/", BaseTestCase.get_trick_word("other/")}),
         ("one/", set()),
+        ("other/", set()),
+    )
+    @BaseTestCase.parametrize_context()
+    def test(
+        self,
+        tmpdir: local,
+        cword: int,
+        words_before: List[str],
+        word: str,
+        words_after: List[str],
+        result: Set[str],
+    ) -> None:
+        self.run(
+            tmpdir=tmpdir,
+            cword=cword,
+            words_before=words_before,
+            word=word,
+            words_after=words_after,
+            result=result,
+        )
+
+
+class TestOneFile(BaseTestCase):
+    def prepare(self, tmpdir: local) -> None:
+        with open(tmpdir.join("one.json"), "w") as fp:
+            fp.write("{}")
+
+    @BaseTestCase.parametrize(
+        ("", {*args(), "one.json"}),
+        ("-", args("-")),
+        ("--", args("--")),
+        ("--he", args("--help")),
+        ("--help", args("--help")),
+        ("t", set()),
+        ("o", {"one.json"}),
+        ("one", {"one.json"}),
+        ("one.json", {"one.json"}),
+    )
+    @BaseTestCase.parametrize_context()
+    def test(
+        self,
+        tmpdir: local,
+        cword: int,
+        words_before: List[str],
+        word: str,
+        words_after: List[str],
+        result: Set[str],
+    ) -> None:
+        self.run(
+            tmpdir=tmpdir,
+            cword=cword,
+            words_before=words_before,
+            word=word,
+            words_after=words_after,
+            result=result,
+        )
+
+
+class TestTwoDirsOneFile(BaseTestCase):
+    def prepare(self, tmpdir: local) -> None:
+        tmpdir.mkdir("one")
+        tmpdir.mkdir("other")
+        with open(tmpdir.join("one.json"), "w") as fp:
+            fp.write("{}")
+
+    @BaseTestCase.parametrize(
+        ("", {*args(), "one/", "other/", "one.json"}),
+        ("-", args("-")),
+        ("--", args("--")),
+        ("--he", args("--help")),
+        ("--help", args("--help")),
+        ("t", set()),
+        ("o", {"one/", "other/", "one.json"}),
+        ("one", {"one/", "one.json"}),
+        ("other", {"other/", BaseTestCase.get_trick_word("other/")}),
+        ("one/", set()),
+        ("other/", set()),
+        ("one.json", {"one.json"}),
     )
     @BaseTestCase.parametrize_context()
     def test(
