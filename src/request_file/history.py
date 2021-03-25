496284a717f8a4880cf1a5bfbe83658494773801
Ashleigh Udoh <ash@tickitto.com>
2021-03-25 10:48:24 +0000

    added namespacing to input history

--- src/request_file/history.py
+++ src/request_file/history.py
@@ -4,30 +4,44 @@ from request_file.files import read_var, write_var
 
 
 class InputHistory:
-    _inputs: Dict[str, str]
+    _NAMESPACE_KEY = "REQUESTFILE_NAMESPACE"
 
     def __init__(self) -> None:
-        self._inputs: Dict[str, str] = {}
+        self._inputs: Dict[str, Dict[str, str]] = {}
 
     def update_input(self, name: str, value: str, namespace: str = "") -> None:
-        self._inputs[name] = value
+        ns = self._inputs.get(namespace, {})
+        ns[name] = value
+        self._inputs[namespace] = ns
 
     def get_last_input(self, name: str, namespace: str = "") -> str:
-        return self._inputs.get(name, "")
+        ns = self._inputs.get(namespace, {})
+        return ns.get(name, "")
 
     def read_input_file(self, path: str) -> None:
+        namespace: str = ""
         with open(path, "r") as fp:
             for line in fp:
                 try:
                     key, value = read_var(line)
                 except ValueError:
                     continue
-                self._inputs[key] = value
+                if key == InputHistory._NAMESPACE_KEY:
+                    namespace = value
+                    continue
+
+                ns = self._inputs.get(namespace, {})
+                ns[key] = value
+                self._inputs[namespace] = ns
 
     def write_input_file(self, path: str) -> None:
         with open(path, "w") as fp:
-            for key, value in self._inputs.items():
-                line = write_var(key, value)
+            for namespace, ns in self._inputs.items():
+                line = write_var(InputHistory._NAMESPACE_KEY, namespace)
                 fp.write(line)
                 fp.write("\n")
+                for key, value in ns.items():
+                    line = write_var(key, value)
+                    fp.write(line)
+                    fp.write("\n")
             fp.write("\n")
